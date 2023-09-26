06f3b597c185d59f034f11c888effe4a40ffd767
SumailaM <142268062+SumailaM@users.noreply.github.com>
2023-09-26 08:29:10 -0700

    Update script.js

--- script.js
+++ script.js
@@ -9,29 +9,82 @@ $("#currentDay").text(today);
 
 // outlineWorkday is used to in each hour of the Workday
 var outlineWorkday = [
-  { time: "9:00 AM", 
-  event: "" },
-{ time: "10:00 AM", 
-  event: "" },
-{ time: "11:00 AM", 
-  event: "" },
-{ time: "12:00 PM", 
-  event: "" },
-{ time: "1:00 PM", 
-  event: "" },
-{ time: "2 PM", 
-  event: "" },
-{ time: "3 PM", 
-  event: "" },
-{ time: "4 PM", 
-  event: "" },
-{ time: "5 PM", 
-  event: "" },
-];
+{
+  id: "0",
+  hour: "09",
+  time: "09",
+  dayNight: "am",
+  reminder:""
+},
+{
+  id: "1",
+  hour: "10",
+  time: "10",
+  dayNight: "am",
+  reminder:""
+},
+{
+  id: "2",
+  hour: "11",
+  time: "11",
+  dayNight: "am",
+  reminder:""
+},
+{
+  id: "3",
+  hour: "12",
+  time: "12",
+  dayNight: "pm",
+  reminder:""
+},
+{
+  id: "4",
+  hour: "01",
+  time: "01",
+  dayNight: "pm",
+  reminder:""
+},
+{
+  id: "5",
+  hour: "02",
+  time: "02",
+  dayNight: "pm",
+  reminder:""
+},
+{
+  id: "6",
+  hour: "03",
+  time: "03",
+  dayNight: "pm",
+  reminder:""
+},
+{
+  id: "7",
+  hour: "04",
+  time: "04",
+  dayNight: "pm",
+  reminder:""
+},
+{
+  id: "8",
+  hour: "05",
+  time: "05",
+  dayNight: "pm",
+  reminder:""
+},
+{
+  id: "9",
+  hour: "06",
+  time: "06",
+  dayNight: "pm",
+  reminder:""
+},
+  ]
+  
 // Added tocheck for local storage
 var workEvents = JSON.parse(localStorage.getItem("workDay"));
 if (workEvents) {
-  planWorkday = workEvents;
+  outlineWorkday = workEvents;
 }
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
